00a3ec33ff033846da6287a40e122fc640c266e7
crankmosh <dinok@cloudcomp.info>
2020-12-05 10:23:02 -0800

    Update scraperhtmllisted.py
    attempt price fix again

--- scraperhtmllisted.py
+++ scraperhtmllisted.py
@@ -38,7 +38,7 @@ def get_price(url):
         xurl.append(url)
     elif 'bestbuy' in url:
         #pricetag = (soup.find('span',class_="aria-hidden")).text
-        pricetag = (soup.find(span="aria-hidden")).text
+        pricetag = (soup.find("aria-hidden")).text
         if pricetag is not None:
             xprice.append(pricetag)
         else:
